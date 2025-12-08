f753441a0358a8a27d662a67ab433a4a90a3be3b
mrfarmad <farmad@live.ru>
2025-12-08 14:40:29 +0300

    initial edge

--- EDGE_NODE-js/server/src/config/index.js
+++
@@ -1,7 +0,0 @@
-export const config = {
-    port: process.env.PORT || 8090,
-    mqttUrl: process.env.MQTT_URL || '',
-    telemetryIntervalMs: Number(process.env.TELEMETRY_INTERVAL_MS || 3000),
-    pingIntervalMs: Number(process.env.PING_INTERVAL_MS || 10000),
-    environment: process.env.NODE_ENV || 'production'
-  };
